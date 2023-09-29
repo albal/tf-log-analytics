6ff86bdb1f526e0c60d303a8111963be642f363a
Al West <al@tsew.net>
2023-09-29 18:09:31 +0100

    Fix formatting

--- log-analytics-test-bootstrap/rg.tf
+++ log-analytics-test-bootstrap/rg.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "rg" {
   name     = "s185-sandbox-rg"
   location = "westeurope"
   tags = {
-    "Environment"      = "Sandbox"
-    "User"             = "Al West"
+    "Environment" = "Sandbox"
+    "User"        = "Al West"
   }
 }
